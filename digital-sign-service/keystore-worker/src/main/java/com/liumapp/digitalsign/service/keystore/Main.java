703feb9c944151528fb0831f73ce0bf22d972677
liumapp <liumapp.com@gmail.com>
2018-02-26 15:36:28 +0800

    rm unnecessary things of worker

--- digital-sign-service/keystore-worker/src/main/java/com/liumapp/digitalsign/service/keystore/Main.java
+++ digital-sign-service/keystore-worker/src/main/java/com/liumapp/digitalsign/service/keystore/Main.java
@@ -4,11 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/26/18 3:35 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
