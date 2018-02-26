bccafa8cd4c16ed83f79b0e58c6b404a2a410ddd
liumapp <liumapp.com@gmail.com>
2018-02-26 14:53:45 +0800

    ready to add keystore & certificate controller

---
+++ digital-sign-engine/keystore-manager/src/main/java/com/liumapp/digitalsign/engine/keystore/controller/CertificateController.java
@@ -0,0 +1,14 @@
+package com.liumapp.digitalsign.engine.keystore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liumapp on 2/26/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@RestController
+@RequestMapping("certificate")
+public class CertificateController {
+}
