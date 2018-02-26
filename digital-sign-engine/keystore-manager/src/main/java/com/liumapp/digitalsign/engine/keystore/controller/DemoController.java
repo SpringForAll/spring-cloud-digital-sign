b98938c2c26e66f7e0116d2bc6fbf64da0670537
liumapp <liumapp.com@gmail.com>
2018-02-26 10:47:22 +0800

    add demo controller for keystore

---
+++ digital-sign-engine/keystore-manager/src/main/java/com/liumapp/digitalsign/engine/keystore/controller/DemoController.java
@@ -0,0 +1,20 @@
+package com.liumapp.digitalsign.engine.keystore.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liumapp on 2/26/18 10:42 AM.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @RequestMapping("")
+    public String index () {
+        return "success";
+    }
+
+}
