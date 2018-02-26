9bf32749ddf156532fb7d495b42f9171bbd3fb9d
liumapp <liumapp.com@gmail.com>
2018-02-26 16:07:40 +0800

    simple test done

--- digital-sign-service/keystore-worker/src/main/java/com/liumapp/digitalsign/service/keystore/KeySotreWorker.java
+++ digital-sign-service/keystore-worker/src/main/java/com/liumapp/digitalsign/service/keystore/KeySotreWorker.java
@@ -11,10 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @EnableDiscoveryClient
 @SpringBootApplication
-public class Main {
+public class KeySotreWorker {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(KeySotreWorker.class , args);
     }
 
 }
