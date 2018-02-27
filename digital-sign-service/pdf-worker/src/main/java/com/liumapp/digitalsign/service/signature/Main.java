55d41402966bfe1254bb689d6384005baf18e717
liumapp <liumapp.com@gmail.com>
2018-02-27 14:54:36 +0800

    rm unnecessary class from archetype

--- digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/Main.java
+++ digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/Main.java
@@ -2,13 +2,15 @@ package com.liumapp.digitalsign.service.signature;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/27/18 2:53 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@EnableDiscoveryClient
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
--- digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/config/Config.java
+++ digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/config/Config.java
@@ -4,12 +4,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ImportResource;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/27/18 2:53 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
 @Configuration
-@ImportResource(locations = {"classpath:/spring/applicationContext*.xml"})
 public class Config {
 
 
