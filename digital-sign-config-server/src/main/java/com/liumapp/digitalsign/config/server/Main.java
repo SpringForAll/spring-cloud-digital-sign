fdb6a8d42963c9792522a791e91ace460ce9bd99
liumapp <liumapp.com@gmail.com>
2018-02-28 15:22:33 +0800

    add config dependency & rm unnecessary files

--- digital-sign-config-server/src/main/java/com/liumapp/digitalsign/config/server/Main.java
+++ digital-sign-config-server/src/main/java/com/liumapp/digitalsign/config/server/Main.java
@@ -2,13 +2,17 @@ package com.liumapp.digitalsign.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
  * Created by liumapp on 9/28/17.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@EnableDiscoveryClient
+@EnableConfigServer
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
