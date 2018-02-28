a0cfe03b654334d41f98deb75f98a385430fd039
liumapp <liumapp.com@gmail.com>
2018-02-28 15:25:42 +0800

    conf config server

--- digital-sign-config-server/src/main/java/com/liumapp/digitalsign/config/server/Main.java
+++ digital-sign-config-server/src/main/java/com/liumapp/digitalsign/config/server/Main.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 2/28/18 3:25 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
