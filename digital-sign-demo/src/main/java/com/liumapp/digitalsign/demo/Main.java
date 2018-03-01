6f90c12bb32421e42fc56efc8cbce3607b43ac17
liumapp <liumapp.com@gmail.com>
2018-03-01 17:23:38 +0800

    config demo

--- digital-sign-demo/src/main/java/com/liumapp/digitalsign/demo/Main.java
+++ digital-sign-demo/src/main/java/com/liumapp/digitalsign/demo/Main.java
@@ -2,13 +2,15 @@ package com.liumapp.digitalsign.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * Created by liumapp on 9/28/17.
+ * Created by liumapp on 3/1/18 5:23 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-@SpringBootApplication(scanBasePackages = {"com.liumapp"})
+@SpringBootApplication
+@EnableDiscoveryClient
 public class Main {
 
     public static void main(String[] args) {
