5f206da6d2ccf242e7369150faa096c46a46b5b5
liumapp <liumapp.com@gmail.com>
2018-03-02 09:19:28 +0800

    add http client util

--- digital-sign-demo/src/main/java/com/liumapp/digitalsign/demo/Demo.java
+++ digital-sign-demo/src/main/java/com/liumapp/digitalsign/demo/Demo.java
@@ -11,10 +11,10 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
  */
 @SpringBootApplication
 @EnableDiscoveryClient
-public class Main {
+public class Demo {
 
     public static void main(String[] args) {
-        SpringApplication.run(Main.class , args);
+        SpringApplication.run(Demo.class , args);
     }
 
 }
---
+++ digital-sign-demo/src/main/java/com/liumapp/digitalsign/demo/utils/HttpClient.java
@@ -0,0 +1,11 @@
+package com.liumapp.digitalsign.demo.utils;
+
+/**
+ * Created by liumapp on 3/2/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+public class HttpClient {
+
+
+}
