bb167280c6293fa015b485d2c8df1a54276e8fea
liumapp <liumapp.com@gmail.com>
2018-03-05 11:33:40 +0800

    add sign method in engine

--- digital-sign-engine/sign-helper/src/main/java/com/liumapp/digitalsign/engine/signature/helper/utils/Sign.java
+++ digital-sign-engine/sign-helper/src/main/java/com/liumapp/digitalsign/engine/signature/helper/utils/Sign.java
@@ -1,69 +1,87 @@
 package com.liumapp.digitalsign.engine.signature.helper.utils;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.security.KeyStore;
-import java.security.PrivateKey;
-import java.security.cert.Certificate;
+import java.io.*;
 
 import com.itextpdf.text.Image;
-import com.itextpdf.text.Rectangle;
 import com.itextpdf.text.pdf.PdfReader;
 import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.PdfStamper;
+import com.itextpdf.text.pdf.security.*;
 
 /**
  * Created by liumapp on 2/27/18 2:35 PM.
  * E-mail:liumapp.com@gmail.com
  * home-page:http://www.liumapp.com
  */
-public class Sign{
-	
-	// 证书密码
-	private static final String cert_pwd = "111111";
-	//利用keytool生成数字证书
-	//keytool -genkey -alias ctidcert -keystore c:/证书名称.keystore -storepass "888" -keypass "888" -keyalg "RSA" -dname "CN=www.123.cn,OU=简称,O=公司名称,L=北京,ST=北京,C=中国"
-	private static final String cert_path = "/usr/local/tomcat/project/pfxSigner/data/liumapp.keystore";
-	//"D:\\dev\\eclipse-jee-juno-SR2-win32\\eclipse\\workspace\\sign\\src\\machunlin_1.pfx";
+public class Sign {
 
-	private static final String image_path = "/usr/local/tomcat/project/pfxSigner/data/sign.png";
-
-	private static final String source_pdf = "/usr/local/tomcat/project/pfxSigner/data/test.pdf";
-
-	private static final String output_pdf = "/usr/local/tomcat/project/pfxSigner/data/output.pdf";
-	
-	public static void main(String[] args) {
+	/**
+	 * 单多次签章通用
+	 * @param src string source pdf
+	 * @param target string target pdf
+	 * @param signatureInfos SignatureInfo
+	 */
+	public static void sign(String src, String target, SignatureInfo... signatureInfos){
+		InputStream inputStream = null;
+		FileOutputStream outputStream = null;
+		ByteArrayOutputStream result = new ByteArrayOutputStream();
 		try {
-			sign();
+			inputStream = new FileInputStream(src);
+			for (SignatureInfo signatureInfo : signatureInfos) {
+				ByteArrayOutputStream tempArrayOutputStream = new ByteArrayOutputStream();
+				PdfReader reader = new PdfReader(inputStream);
+				//创建签章工具PdfStamper ，最后一个boolean参数是否允许被追加签名
+				PdfStamper stamper = PdfStamper.createSignature(reader, tempArrayOutputStream, '\0', null, true);
+				// 获取数字签章属性对象
+				PdfSignatureAppearance appearance = stamper.getSignatureAppearance();
+				appearance.setReason(signatureInfo.getReason());
+				appearance.setLocation(signatureInfo.getLocation());
+				//设置签名的签名域名称，多次追加签名的时候，签名预名称不能一样，图片大小受表单域大小影响（过小导致压缩）
+				appearance.setVisibleSignature(signatureInfo.getFieldName());
+				//读取图章图片
+				Image image = Image.getInstance(signatureInfo.getImagePath());
+				appearance.setSignatureGraphic(image);
+				appearance.setCertificationLevel(signatureInfo.getCertificationLevel());
+				//设置图章的显示方式，如下选择的是只显示图章（还有其他的模式，可以图章和签名描述一同显示）
+				appearance.setRenderingMode(signatureInfo.getRenderingMode());
+				/**
+				 * 粘贴图片
+				 */
+//                PdfContentByte over = stamper.getOverContent(1);
+//                image.setAbsolutePosition(0 , 0);
+//                image.scaleToFit(300 , 109);
+//                over.addImage(image);
+				// 摘要算法
+				ExternalDigest digest = new BouncyCastleDigest();
+				// 签名算法
+				ExternalSignature signature = new PrivateKeySignature(signatureInfo.getPk(), signatureInfo.getDigestAlgorithm(), null);
+				// 调用itext签名方法完成pdf签章
+				MakeSignature.signDetached(appearance, digest, signature, signatureInfo.getChain(), null, null, null, 0, signatureInfo.getSubfilter());
+				//定义输入流为生成的输出流内容，以完成多次签章的过程
+				inputStream = new ByteArrayInputStream(tempArrayOutputStream.toByteArray());
+				result = tempArrayOutputStream;
+			}
+			outputStream = new FileOutputStream(new File(target));
+			outputStream.write(result.toByteArray());
+			outputStream.flush();
 		} catch (Exception e) {
-			System.out.println(e.getMessage());
-		}  
-		System.out.println("done!!!");
+			e.printStackTrace();
+		} finally {
+			try {
+				if(null!=outputStream){
+					outputStream.close();
+				}
+				if(null!=inputStream){
+					inputStream.close();
+				}
+				if(null!=result){
+					result.close();
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
-	
-	public static void sign() throws Exception {
-
-		KeyStore ks = KeyStore.getInstance("jks");
-		ks.load(new FileInputStream(cert_path), cert_pwd.toCharArray());
-		String alias = (String)ks.aliases().nextElement();
-		PrivateKey key = (PrivateKey)ks.getKey(alias, cert_pwd.toCharArray());
-		Certificate[] chain = ks.getCertificateChain(alias);
-		PdfReader reader = new PdfReader(source_pdf);
-		FileOutputStream fout = new FileOutputStream(output_pdf);
-		PdfStamper stp = PdfStamper.createSignature(reader, fout, '\0');
-
-		PdfSignatureAppearance sap = stp.getSignatureAppearance();
-//		sap.setCrypto(key, chain, null, PdfSignatureAppearance.WINCER_SIGNED);
-		sap.setReason("I'm the author");
-		sap.setLocation("Ma");
-		sap.setImage(Image.getInstance(image_path));
-		sap.setCertificationLevel(PdfSignatureAppearance.CERTIFIED_NO_CHANGES_ALLOWED);
-		Rectangle r = new Rectangle(100, 100, 300,109);
-		sap.setVisibleSignature(r, 1, null);
-		stp.close();
-
-	}
-
 
 
 }
--- digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/controller/SignController.java
+++ digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/controller/SignController.java
@@ -2,6 +2,8 @@ package com.liumapp.digitalsign.service.signature.controller;
 
 import com.itextpdf.text.pdf.PdfSignatureAppearance;
 import com.itextpdf.text.pdf.security.DigestAlgorithms;
+import com.liumapp.digitalsign.engine.signature.helper.utils.Sign;
+import com.liumapp.digitalsign.engine.signature.helper.utils.SignatureInfo;
 import com.liumapp.digitalsign.service.signature.pattern.PdfSignPattern;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
@@ -27,28 +29,37 @@ public class SignController {
     @Value("${keyStoreSavePath}")
     private String keyStoreSavePath;
 
+    @Value("${sign.reason}")
+    private String reason;
+
+    @Value("${sign.location}")
+    private String location;
+
+    @Value("${tmpDir}")
+    private String tmpDir;
+
     @RequestMapping("")
     public ResponseEntity<?> sign (@RequestBody PdfSignPattern pdfSignPattern) {
         try {
             String tmpPdfOut = pdfSignPattern.getTmpPdf() + "final";
 
             KeyStore ks = KeyStore.getInstance("jks");
-            ks.load(new FileInputStream(keyStoreSavePath + "/" + "keystore.ks") , params.getKeyStorePd().toCharArray());
+            ks.load(new FileInputStream(keyStoreSavePath + "/" + pdfSignPattern.getKeystore()) , pdfSignPattern.getKeystorePd().toCharArray());
             PrivateKey pk = (PrivateKey) ks.getKey(pdfSignPattern.getAlias() , pdfSignPattern.getCertPd().toCharArray());
             Certificate[] chain = ks.getCertificateChain(pdfSignPattern.getAlias());
 
             SignatureInfo signatureInfo = new SignatureInfo();
-            signatureInfo.setReason("数字签名");
-            signatureInfo.setLocation("浙江葫芦娃网络技术有限公司");
+            signatureInfo.setReason(reason);
+            signatureInfo.setLocation(location);
             signatureInfo.setPk(pk);
             signatureInfo.setChain(chain);
             signatureInfo.setCertificationLevel(PdfSignatureAppearance.NOT_CERTIFIED);
             signatureInfo.setDigestAlgorithm(DigestAlgorithms.SHA256);
             signatureInfo.setFieldName(pdfSignPattern.getSignatureField());
-            signatureInfo.setImagePath(params.getTmpDir() + "/" + pdfSignPattern.getTmpImg());
+            signatureInfo.setImagePath(tmpDir + "/" + pdfSignPattern.getTmpImg());
             signatureInfo.setRenderingMode(PdfSignatureAppearance.RenderingMode.GRAPHIC);
 
-            sign(params.getTmpDir() + "/" + pdfSignPattern.getTmpPdf() , params.getTmpDir() + "/" + tmpPdfOut , signatureInfo);
+            Sign.sign(tmpDir + pdfSignPattern.getTmpPdf() , tmpDir + tmpPdfOut , signatureInfo);
             ossUtil.uploadFile("signed" + pdfSignPattern.getPdfKey() , new File(params.getTmpDir() + "/" + tmpPdfOut));
 
             fileUtil.deleteFile(params.getTmpDir() + "/" + pdfSignPattern.getTmpImg());
