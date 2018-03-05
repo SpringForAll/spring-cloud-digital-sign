179fb13cab3a21cf5dd6f7a1e225c1b5c0964c60
liumapp <liumapp.com@gmail.com>
2018-03-05 11:17:11 +0800

    add sign controller for sign-worker

---
+++ digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/controller/SignController.java
@@ -0,0 +1,17 @@
+package com.liumapp.digitalsign.service.signature.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by liumapp on 3/5/18.
+ * E-mail:liumapp.com@gmail.com
+ * home-page:http://www.liumapp.com
+ */
+@RestController
+@RequestMapping("sign")
+public class SignController {
+
+    
+
+}
