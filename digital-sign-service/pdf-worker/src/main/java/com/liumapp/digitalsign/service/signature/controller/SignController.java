6b0c76635656ef6269e822c2f9c1c23825b2dc77
liumapp <liumapp.com@gmail.com>
2018-03-05 11:21:18 +0800

    add sign pattern

--- digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/controller/SignController.java
+++ digital-sign-service/pdf-worker/src/main/java/com/liumapp/digitalsign/service/signature/controller/SignController.java
@@ -1,5 +1,8 @@
 package com.liumapp.digitalsign.service.signature.controller;
 
+import com.liumapp.digitalsign.service.signature.pattern.PdfSignPattern;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +15,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("sign")
 public class SignController {
 
-    
+    @RequestMapping("")
+    public ResponseEntity<?> sign (@RequestBody PdfSignPattern pdfSignPattern) {
+        
+
+        return ResponseEntity.ok("success");
+    }
 
 }
