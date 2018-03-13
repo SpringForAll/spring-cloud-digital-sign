d73e7871f1895382dc29dda4c6e0b2ec32a23162
liumapp <liumapp.com@gmail.com>
2018-03-13 14:35:49 +0800

    add build images for docker shell

---
+++ build-images.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+echo '============================================================='
+echo '$                                                           $'
+echo '$                      liumapp                              $'
+echo '$                                                           $'
+echo '$                                                           $'
+echo '$  email:    liumapp.com@gmail.com                          $'
+echo '$  homePage: http://www.liumapp.com                         $'
+echo '$  Github:   https://github.com/liumapp                     $'
+echo '$                                                           $'
+echo '============================================================='
+echo '.'
+
+cd digital-sign-config-server
+
+mvn clean package docker:build
+
+cd ..
+
+cd digital-sign-demo
+
+mvn clean package docker:build
+
+cd ..
+
+cd digital-sign-eureka
+
+mvn clean package docker:build
+
+cd ..
+
+cd digital-sign-gateway
+
+mvn clean package docker:build
+
+cd ..
+
+cd digital-sign-service/keystore-worker
+
+mvn clean package docker:build
+
+cd ../..
+
+cd digital-sign-service/pdf-worker
+
+cd ../../
