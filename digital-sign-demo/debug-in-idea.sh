f1bd0f449eda0fdb7b7c8c46e1b1ce432b033190
liumapp <liumapp.com@gmail.com>
2018-03-16 14:27:25 +0800

    wrong test

--- digital-sign-demo/debug-in-idea.sh
+++ digital-sign-demo/debug-in-idea.sh
@@ -12,7 +12,7 @@ echo '$                                                           $'
 echo '============================================================='
 echo '.'
 
-docker run -d -e "JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n" -p 8080:8080 -p 8000:8000 --name sign-demo liumapp/digital-sign-demo:v1.0.0 -h sign-demo
+docker run -e "JAVA_OPTS=-agentlib:jdwp=transport=dt_socket,address=8000,server=y,suspend=n" -p 8000:8000 -p 8080:8080 --name sign-demo liumapp/digital-sign-demo:v1.0.0 --hostname sign-demo
 
 
 
