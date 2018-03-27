e1775405c5f4ca26af9948727ec00571bb776949
liumapp <liumapp.com@gmail.com>
2018-03-27 10:49:33 +0800

    config cert util

---
+++ digital-sign-engine/certi-util/src/main/java/com/liumapp/digitalsign/engine/cert/util/CertUtil.java
@@ -0,0 +1,16 @@
+package com.liumapp.digitalsign.engine.cert.util;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author liumapp
+ * @file Main.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 3/27/18
+ */
+@Configuration
+public class CertUtil {
+
+
+}
---
+++ digital-sign-engine/certi-util/src/main/java/com/liumapp/digitalsign/engine/cert/util/alias/AliasGenerator.java
@@ -0,0 +1,19 @@
+package com.liumapp.digitalsign.engine.cert.util.alias;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liumapp
+ * @file AliasGenerator.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 3/27/18
+ * The alias of the certificate is saved on the basis of the serial number of the certificate to 16.
+ * the password equals the keystore password .
+ */
+@Component
+public class AliasGenerator {
+
+
+
+}
