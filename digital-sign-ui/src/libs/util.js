f0320700799fa8376bc7e6008bcb17b72a2620da
liumapp <liumapp.com@gmail.com>
2018-07-18 09:19:21 +0800

    import iview and config

---
+++ digital-sign-ui/src/libs/util.js
@@ -0,0 +1,72 @@
+/**
+ * @file util.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/18/18
+ */
+
+import axios from 'axios';
+import Cookies from 'js-cookie';
+import { Message } from 'iview';
+
+let util = {
+
+};
+util.title = function (title) {
+  title = title || 'digital sign demo';
+  window.document.title = title;
+};
+
+const ajaxUrl = 'http://localhost:2020/';
+
+util.ajaxUrl = ajaxUrl;
+
+util.ajax = axios.create({
+  baseURL: ajaxUrl,
+  timeout: 30000
+});
+
+util.post = function (url, data) {
+  return axios({
+    method: 'post',
+    baseURL: ajaxUrl,
+    url,
+    data: data,
+    timeout: 10000,
+    headers: {
+      'X-Requested-With': 'XMLHttpRequest',
+      'Content-Type': 'application/json; charset=UTF-8',
+    }
+  }).then(
+    (response) => {
+      return this.checkStatus(response);
+    }
+  )
+};
+
+util.checkStatus = function checkStatus (response) {
+  // loading
+  // 如果http状态码正常，则直接返回数据
+  // console.log(response)
+  if (response && (response.status === 200 || response.status === 304 || response.status === 400)) {
+    return response;
+    // 如果不需要除了data之外的数据，可以直接 return response.data
+  }
+  Message.warning('网络异常');
+  // 异常状态下，把错误信息返回去
+  return {
+    status: -404,
+    msg: '网络异常'
+  };
+};
+
+util.randNumber = function (n) {
+  let rnd = "";
+  for (let i = 0 ; i < n ; i++) {
+    rnd += Math.floor(Math.random()*10);
+  }
+  return rnd;
+};
+
+export default util;
--- digital-sign-ui/src/main.js
+++ digital-sign-ui/src/main.js
@@ -1,15 +1,23 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+/**
+ * @file main.js
+ * @author liumapp
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 7/18/18
+ */
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import iView from 'iview'
+import 'iview/dist/styles/iview.css'
 
 Vue.config.productionTip = false
-
+Vue.use(iView)
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  render: h => h(App),
   components: { App },
   template: '<App/>'
 })
