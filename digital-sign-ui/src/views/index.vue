b5878aed9f2fd74a4a22553b7d55b62d4b2a0f8e
liumapp <liumapp.com@gmail.com>
2018-07-18 09:33:18 +0800

    define 4 steps as : upload doc , upload signer info , confirm signature info , download results doc

--- digital-sign-ui/src/views/index.vue
+++ digital-sign-ui/src/views/index.vue
@@ -12,8 +12,9 @@
       <Card>
         <Steps :current="current">
           <Step title="上传合同文档" content="上传一个doc或PDF文档"></Step>
-          <Step title="填写签署人" content="上传参与合同签署人的相关信息，至少两人"></Step>
-          <Step title="填写签署人" content="上传参与合同签署人的相关信息，至少两人"></Step>
+          <Step title="填写签署人" content="上传参与合同签署人的相关信息，限制为两人"></Step>
+          <Step title="签署信息确认" content="后端自动生成签署人证书信息、签署位置、签章图片等相关信息"></Step>
+          <Step title="下载合同文档" content="下载带有双方证书签名的合同文档"></Step>
         </Steps>
         <doc-upload v-if="current == 0" @next="nextStep" @setDocData="setDocData" :convertId="convertId"></doc-upload>
         <converting v-if="current == 1" :docList="docList" @next="nextStep" :convertId="convertId"></converting>
