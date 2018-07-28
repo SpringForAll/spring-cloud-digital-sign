3e3ec7717ad6fefbef803242bda6c97e7a939336
liumapp <liumapp.com@gmail.com>
2018-07-28 19:32:31 +0800

    update init-jks components

--- digital-sign-ui/src/components/init-jks.vue
+++ digital-sign-ui/src/components/init-jks.vue
@@ -6,9 +6,35 @@
  * @date 7/23/18
  */
 <template>
+  <Form ref="initKsForm" :model="checkMsgForm" :rules="checkMsgRule">
+    <FormItem label="用户名" prop="name">
+      <Input type="text" v-model="checkMsgForm.name"></Input>
+    </FormItem>
+    <FormItem label="年龄" prop="age">
+      <Input type="text" v-model="checkMsgForm.age" number></Input>
+    </FormItem>
+    <FormItem label="头像" prop="headPic">
+      <SingleImgUpload id="head-pic-upload"
+                       :uploadUrl="uploadUrl"
+                       @imgInfo="handlePic" ></SingleImgUpload>
+    </FormItem>
+    <FormItem>
+      <Button type="primary" @click="handleSubmit('checkMsgForm')">Submit</Button>
+      <Button type="ghost" @click="handleReset('checkMsgForm')">Reset</Button>
+    </FormItem>
+  </Form>
 </template>
 <script>
+import util from '@/libs/util'
 export default {
-  name: 'init-jks'
+  name: 'init-jks',
+  data () {
+    return {
+      initKsForm: {
+        name: 'KeyStore Name',
+
+      }
+    }
+  },
 }
 </script>
